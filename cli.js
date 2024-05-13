907bd15a98c32eee7d4ccec590cce30a991c4539
Jainam <jainam@02042024>
2024-05-13 17:43:27 +0530

    initialize CLI utility

---
+++ cli.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const resource = process.argv[2];
+const resourceName = resource.charAt(0).toUpperCase() + resource.slice(1);
+
+// write controller file
+// write model file
+// write route file
